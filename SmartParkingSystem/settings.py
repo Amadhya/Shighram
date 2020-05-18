c078aed297aa3a20d1467a9206f8ec431740573c
Amadhya <eshuanand07@gmail.com>
2020-05-18 21:26:47 +0530

    heroku deployment

--- SmartParkingSystem/settings.py
+++ SmartParkingSystem/settings.py
@@ -33,7 +33,7 @@ GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '#GOOGLE_CLIENT_ID')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', 'http://suvidham.now.sh', 'https://suvidham.now.sh/login']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', 'http://suvidham.now.sh', 'https://suvidham.now.sh','suvidham.now.sh']
 
 
 # Application definition
